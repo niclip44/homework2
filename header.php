d6fe11c02debd370e8b1504d390d0620d579c4d6
niclip44 <97993169+niclip44@users.noreply.github.com>
2022-09-07 14:25:24 -0500

    Update header.php

--- header.php
+++ header.php
@@ -45,7 +45,7 @@
   <body>
     <h1>Hello, world!</h1>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
-<form action="handlepost.php" method="post">
+<form action="handlepost.php" method="get">
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
 <input type="submit">
