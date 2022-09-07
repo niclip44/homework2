d5e3f7b96b937b233b0c94719a5eb0ba6c5a7835
niclip44 <97993169+niclip44@users.noreply.github.com>
2022-09-07 14:14:19 -0500

    Update header.php

--- header.php
+++ header.php
@@ -45,11 +45,5 @@
   </div>
 </nav>
 
-<form action="welcome.php" method="post">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
-<input type="submit">
-</form>
-
 </body>
 </html>
