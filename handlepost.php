510b575b6e044809ff1e6e29a2d7676054fea98f
niclip44 <97993169+niclip44@users.noreply.github.com>
2022-09-18 23:11:59 -0500

    Update handlepost.php

--- handlepost.php
+++ handlepost.php
@@ -4,8 +4,7 @@
 Welcome <?php echo $_POST["name"]; ?><br>
 Your email address is: <?php echo $_POST["email"]; ?><br>
   
-You are <?php echo $_GET["age"]; ?><br>
-Your school is <?php echo $_GET["school"]; ?>
+
 
 </body>
 </html>
