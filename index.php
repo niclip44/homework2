4e67d88e523cc5b530b8b3ced797bc032dc5936c
niclip44 <97993169+niclip44@users.noreply.github.com>
2022-09-18 23:05:27 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <form action="handlepost.php" method="get">
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
-  </form>
+  
 <form action="handlepost.php" method="post">
   Age: <input type="text" name="age"><br>
 school: <input type="text" name="school"><br>
