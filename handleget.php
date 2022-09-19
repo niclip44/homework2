dac1b04f097aa01ca8a1106d7f86b87bf0e15f8a
niclip44 <97993169+niclip44@users.noreply.github.com>
2022-09-18 23:19:09 -0500

    Create handleget.php

---
+++ handleget.php
@@ -0,0 +1,9 @@
+<html>
+  <body>
+<form action="handlepost.php" method="post">
+Age: <input type="text" name="age"><br>
+School: <input type="text" name="school"><br>
+<input type="submit">
+</form>
+  </body>
+</html>
