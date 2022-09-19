78c5357344ac3141efa08f2e33a5ce7623b154e3
niclip44 <97993169+niclip44@users.noreply.github.com>
2022-09-18 23:02:50 -0500

    Update header.php

--- header.php
+++ header.php
@@ -46,9 +46,6 @@
    <form action="handlepost.php" method="get">
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
-Age: <input type="text" name="age"><br>
-school: <input type="text" name="school"><br>
-<input type="submit">
 </form>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 
